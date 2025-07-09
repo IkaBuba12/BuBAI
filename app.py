90204fc6e033aaaafed74e745f181a147f5c536f
IkaBuba12 <136633543+IkaBuba12@users.noreply.github.com>
2025-07-09 16:31:54 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,4 @@ from ext import app
 from routes import index, login,register,load_user,logout,chat
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+app.run(debug=True, host='0.0.0.0')
